e063cdca7534c60d8af446a60ca0e8e4882cb285
James Bradbury <jamesbradbury93@gmail.com>
2019-06-13 23:39:53 +0100

    Parallelise everything with shared dict

--- cull_weak.py
+++ cull_weak.py
@@ -8,37 +8,46 @@ import numpy as np
 from scipy.io import wavfile
 import random
 import time
+import sys
 
 root = get_path()
 unique_audio_folder = f'{root}/DataAudioUnique'
 unique_audio_files = os.listdir(unique_audio_folder)
+unique_audio_files.remove('.DS_Store')
 tmp = f'{root}/tmp'
 loudness_json = f'{root}/unique_audio_loudness.json'
-loudness_dict = dict()
 
+loudness_dict = mp.Manager().dict()
 
 def analyse(idx):
     src = f'{unique_audio_folder}/{unique_audio_files[idx]}'
     key = unique_audio_files[idx]
-    # src = '/Users/jamesbradbury/desktop/browse.VC.db-wal.wav_242.wav'
-    new_hash = random.getrandbits(128)
-    loudness = f'{tmp}/loudness{new_hash}.wav'
-    stats = f'{tmp}/stats{new_hash}.wav'
+    loudness = f'{tmp}/loudness{key}.wav'
+    stats = f'{tmp}/stats{key}.wav'
     subprocess.call(['loudness', '-source', src, '-features', loudness, '-windowsize', '17640', '-hopsize', '4410'])
     subprocess.call(['stats', '-source', loudness, '-stats', stats, '-numderivs', '1'])
-    sr, data = wavfile.read(stats)
-    loudness_dict[key] = data[0][0]
+    try:
+        sr, data = wavfile.read(stats)
+    except FileNotFoundError:
+        print(f'I was not able to find the file: {key}')
+    # loudness_dict[key] = data[0][0]
+    loudness_dict[key] = float(data[0][0])
     os.remove(loudness)
     os.remove(stats)
-    
-    
+
 start = time.time()
 pool = mp.Pool() #use all available cores, otherwise specify the number you want as an argument
-pool.map(analyse, range(len(unique_audio_files)))
+num_jobs = len(unique_audio_files)
+for i, _ in enumerate(
+    pool.imap_unordered(analyse, range(num_jobs))
+    , 1):
+    sys.stderr.write('\rdone {0:%}'.format(i/num_jobs))
+        
+output_dict = dict(loudness_dict)
 
-## Dump as JSON
 with open(loudness_json, 'w') as fp:
-    json.dump(loudness_dict, fp, indent=4)
+    json.dump(output_dict, fp, indent=4)
+
 end = time.time()
 time_taken = (end - start) / 60.
 print('Loudness measurements complete.')
