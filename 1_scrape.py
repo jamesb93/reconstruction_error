41fda197d5dd2ee8598365d6b6f145818058cfd6
James Bradbury <jamesbradbury93@gmail.com>
2019-06-13 11:52:27 +0100

    rename

--- 1_scrape.py
+++ 1_scrape.py
@@ -12,13 +12,14 @@ def mass_crawl():
     for root, dirs, files in crawler:
         for x in files:
             # Make paths/dirs
+            x = x.replace(' ', '_')
             full_path = os.path.join(root, x)
             raw_path = os.path.join(raw_dir, f'{x}.raw')
             audio_path = os.path.join(audio_dir, f'{x}.wav')
             
             # Do checks
             if current_size < mb_lim:
-                if check_size(full_path) and check_ext(full_path, bad_exts):
+                if check_size(full_path, 30000) and check_ext(full_path, bad_exts):
                     copyfile(full_path, raw_path) 
                     current_size += bytes_to_mb(os.path.getsize(raw_path))
                     if current_size < mb_lim:
@@ -27,11 +28,8 @@ def mass_crawl():
 
 raw_dir   = '/Users/jamesbradbury/dev/data_bending/DataCopy/'
 audio_dir = '/Users/jamesbradbury/dev/data_bending/DataAudio/'
-mb_lim = 10000
+mb_lim = 5000
 bad_exts = ['.mov', '.avi', '.wmv', '.webm', '.flv', '.sock', '.wav', '.aiff', '.aif', '.mp3', '.mp4', '.wav', '.flac']
 wipe_dir(raw_dir)
 wipe_dir(audio_dir)
 mass_crawl()
-
-# print(check_ext('/Users/jamesbradbury/Sync/0_Max/Data/AEC.pdf', bad_exts))
-# print(check_size('/Users/jamesbradbury/Library/Saved Application State/com.giorgiocalderolla.Wipr-Mac.savedState'))
