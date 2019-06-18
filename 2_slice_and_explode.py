f03a8facf4082360324071169a48cd1f15d7ab79
James Bradbury <jamesbradbury93@gmail.com>
2019-06-18 15:05:07 +0100

    update with new flucoma tools

--- 2_slice_and_explode.py
+++ 2_slice_and_explode.py
@@ -21,21 +21,20 @@ tmp = os.path.join(root, 'tmp')
 
 wipe_dir(unique_audio_folder)
 wipe_dir(tmp)
+## The actual function we will call to do novelty slicing and sample exploding
 def explode(idx):
     ## NoveltySlice on File
     novelty_src = os.path.join(audio_folder, audio_files[idx])
     novelty_indices = os.path.join(tmp, f'{audio_files[idx]}_slices.wav')
-    subprocess.call(['rtnoveltyslice', '-source', novelty_src, '-indices', novelty_indices, '-fftsettings', '2048', '1024', '2048', '-threshold', '0.61', '-kernelsize', '3', '-filtersize', '1'])
+    subprocess.call(['noveltyslice', '-source', novelty_src, '-indices', novelty_indices, '-fftsettings', '2048', '1024', '2048', '-threshold', '0.61', '-kernelsize', '3', '-filtersize', '1'])
     ## Turn slices wav into a list
-    try:
-        sr, data = wavfile.read(novelty_indices)
-    except FileNotFoundError:
-        print(f'I was not able to find the file: {novelty_indices}')
-    
+    data = bufspill(novelty_indices)
+    sound = bufspill(novelty_src)
+    book_end = len(sound) ## We get the length of the buffer and keep this for later
     t_audio = AudioSegment.from_wav(novelty_src)
     no_ext = os.path.basename(novelty_src)
     no_ext = os.path.splitext(no_ext)[0]
-    
+    data = np.append(data, book_end) # Append the length of the original sound file to our slices to create a boundary
     ## Explode to individual files
     for i in range(len(data) - 1):
         start = samps2ms(data[i], 44100)
@@ -43,6 +42,8 @@ def explode(idx):
         t_output = t_audio[start:end]
         t_output.export(os.path.join(unique_audio_folder, f'{no_ext}_{i}.wav'), format='wav')
     os.remove(novelty_indices)
+
+# Start process workers
 start = time.time()
 num_jobs = len(audio_files)
 with mp.Pool() as pool:
@@ -53,7 +54,7 @@ with mp.Pool() as pool:
 
 end = time.time()
 time_taken = round(((end-start) / 60.), 2)
-print('Process complete in:', time_taken)
+print('\nProcess complete in:', time_taken)
 
 
     
