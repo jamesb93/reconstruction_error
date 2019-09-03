163e23b074e75cbc55047df51e222df1fd5591b5
James Bradbury <jamesbradbury93@gmail.com>
2019-09-03 21:57:34 +0100

    move to bokeh plotting

--- python_scripts/dimensionality_reduction/_reduction.py
+++ python_scripts/dimensionality_reduction/_reduction.py
@@ -1,15 +1,16 @@
 import sys
 sys.path.append('../')
 import os
-from databending_utilities import read_json, write_json, norm_np, printp, read_yaml, get_path
-from db_vars import parent, root, analysis_data
-from sklearn import decomposition
-from sklearn import manifold
 import umap
 import numpy as np
-import plotly
-from plotly.offline import plot
-import plotly.graph_objs as go
+import random
+from bokeh.plotting import figure, output_file, show
+from shutil import copyfile
+from datamosh.utils import read_json, write_json, printp, read_yaml, get_path, mkdir
+from datamosh.variables import parent, analysis_data
+from sklearn import decomposition
+from sklearn import manifold
+from sklearn.preprocessing import MinMaxScaler
 from scipy.io import wavfile
 
 if len(sys.argv) != 2:
@@ -19,7 +20,7 @@ if len(sys.argv) != 2:
 this_script = os.getcwd()
 
 # Configuration
-cfg_path = os.path.join(this_script, sys.argv[1])
+cfg_path = os.path.join(this_script, 'configs', sys.argv[1])
 cfg = read_yaml(cfg_path)
 wav_out       = cfg['wav']
 json_out      = cfg['json']
@@ -27,13 +28,21 @@ input_data    = cfg['input_data']
 pre_reduction = cfg['pre_reduction']
 algorithm     = cfg['algorithm']
 tog_plot      = cfg['plot']
+identifier    = cfg['identifier']
 
-feature = read_json(os.path.join(analysis_data, input_data))
+folder_name = f'{algorithm}_{identifier}'
+output_path = os.path.join(this_script, 'outputs', folder_name)
+mkdir(output_path)
+copyfile(cfg_path, os.path.join(output_path, 'configuration.yaml'))
 
-raw_data = [v for v in feature.values()]
-data = np.asarray(raw_data)
-data = norm_np(data)
+feature = read_json(os.path.join(parent, input_data))
+
+data = [v for v in feature.values()]
 keys = [k for k in feature.keys()]
+
+data = np.array(data)
+scaler = MinMaxScaler()
+data = scaler.fit_transform(data)
 
 ######### Initial Reduction ##########
 if pre_reduction != 0:
@@ -50,29 +59,43 @@ if algorithm == 'TSNE':
 if algorithm == 'ISOMAP':
     reduction = manifold.Isomap(n_components=2)
 if algorithm == 'UMAP':
-    reduction = umap.UMAP(n_components=2)
+    umap_neighbours = cfg['umap_neighbours']
+    umap_mindist    = cfg['umap_mindist']
+    reduction = umap.UMAP(n_components=2, n_neighbors=umap_neighbours, min_dist=umap_mindist)
 printp('Fitting Transform')
 data = reduction.fit_transform(data)
+
+# Normalisation
+printp('Normalising for JSON output')
+data = scaler.fit_transform(data)
 
 if tog_plot:
     printp('Transposing Data')
     data_transposed = data.transpose() ## X as one list, Y as another
-    data_transposed = np.ndarray.tolist(data_transposed)
+    # data_transposed = np.ndarray.tolist(data_transposed)
     printp('Plotting')
-    plot_title = f'Reducing {input_data} using the {algorithm} algorithm.'
-    ### Plot ###
-    plot([go.Scattergl(x=data_transposed[0], y=data_transposed[1], mode='markers')])
+    TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
+    plot_output_path = os.path.join('outputs', output_path, f'{plot}.html')
+    output_file(
+        plot_output_path, 
+        title=f'Reduction using the {algorithm} algorithm', 
+        mode="cdn")
+    p = figure(
+        tools=TOOLS,
+        x_range=(0.0, 1.0), 
+        y_range=(0.0, 1.0),
+        plot_width = 800,
+        plot_height= 800,
+        toolbar_location='below',)
+    p.scatter(data_transposed[0], data_transposed[1], radius=0.001, fill_alpha=0.6, line_color=None)
+    show(p)
 
-# Normalisation
-printp('Normalising for JSON output')
-data = norm_np(data)
 out_dict = {}
-
 printp('Outputting JSON')
 for key, value in zip(keys, data):  
     out_dict[key] = value.tolist()
-write_json(os.path.join(this_script, json_out), out_dict)
+write_json(os.path.join('outputs', output_path, json_out), out_dict)
 
 printp('Outputting WAV')
 data = data.astype('float32')
-wavfile.write(os.path.join(this_script, wav_out), 44100, data)
+wavfile.write(os.path.join('outputs', output_path, wav_out), 44100, data)
