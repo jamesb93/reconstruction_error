29bdea95a340b8f1c26f4c04d2efa8622ed90282
James Bradbury <jamesbradbury93@gmail.com>
2019-09-03 23:09:23 +0100

    package update

--- python_scripts/datamosh/osc.py
+++
@@ -1,5 +0,0 @@
-def max_post(console_post, client_obj):
-    '''
-    A wrapper for sending messages back to Max in the console. Makes for easy debugging and GUI.
-    '''
-    client_obj.send_message('/post', console_post)
