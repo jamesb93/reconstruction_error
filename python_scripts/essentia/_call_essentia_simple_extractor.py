e48903c07b769cc38442704e76df274101e3cbf7
James Bradbury <jamesbradbury93@gmail.com>
2019-09-19 14:26:22 +0100

    script for performing multithreaded extractor on audio files

---
+++ python_scripts/essentia/_call_essentia_simple_extractor.py
@@ -0,0 +1,31 @@
+import subprocess as sp
+import multiprocessing as mp
+import os
+import sys 
+import time
+from datamosh.utils import check_make
+from datamosh.variables import unique_audio_folder, unique_audio_files, project_root, essentia_analysis
+
+
+num_jobs = len(unique_audio_files)
+simple_analysis_folder = os.path.join(essentia_analysis, 'simple_extractor')
+check_make(simple_analysis_folder)
+
+def process(idx):
+
+    input_file = os.path.join(unique_audio_folder, unique_audio_files[idx])
+    output_file = os.path.join(simple_analysis_folder, f'{unique_audio_files[idx]}.json')
+
+    if not os.path.isfile(output_file):
+        sp.call([
+            'essentia_databend_simple_extractor', 
+            input_file,
+            output_file
+        ])
+    
+
+with mp.Pool() as pool:
+    for i, _ in enumerate(
+        pool.imap_unordered(process, range(num_jobs))
+        , 1):
+        sys.stderr.write('\rdone {0:%}'.format(i/num_jobs))
