fc2ff7d70390bcbde03ce0e63b85a39e150b2294
James Bradbury <jamesbradbury93@gmail.com>
2019-10-27 17:18:57 +0000

    add args

--- python_scripts/descriptor_analysis/6_mfccs.py
+++ python_scripts/descriptor_analysis/6_mfccs.py
@@ -1,34 +1,40 @@
 import os
 import sys
 import time
+import tempfile
+import argparse
 import subprocess
 import multiprocessing as mp
-import tempfile
-from shutil import copyfile
-from datamosh.utils import bufspill, write_json
-from datamosh.variables import unique_audio_files, unique_audio_folder
+from shutil import copyfile, rmtree
+from datamosh.utils import bufspill, write_json, printp
+from datamosh.variables import unique_audio_files, unique_audio_folder, analysis_data
+
+parser = argparse.ArgumentParser(description='Slice a folder of audio files using fluid-noveltyslice.')
+parser.add_argument('-i', '--infolder', type=str, help='The input folder to analyse')
+parser.add_argument('-o', '--outfile', type=str, help='The output JSON containing analysis')
+args = parser.parse_args()
 
 # You need to pass in the input directory and the output file
-if len(sys.argv) < 2:
-    print('You need to pass an output file.')
+if not args.infolder or not args.outfile:
+    print('You need to provide an input folder/output file.')
     exit()
 
 this_script = os.path.dirname(os.path.realpath(__file__))
 
-output_file = sys.argv[1]
-output_json = os.path.join(this_script, output_file)
-
-# temporary directory for files
+# Prepare for work
 tmp_dir = tempfile.mkdtemp()
+input_folder = args.infolder
+input_files = os.listdir(input_folder)
+output_json = os.path.join(analysis_data, args.outfile)
 
-## Global Dicts for writing out results
+# Global Dicts for writing out results
 mfcc_dict = mp.Manager().dict()
 
 def analyse(idx):
     # Setup paths/files etc
-    mfcc_src = os.path.join(unique_audio_folder, unique_audio_files[idx])
-    mfcc_features = os.path.join(tmp_dir, f'{unique_audio_files[idx]}_features.wav')
-    mfcc_stats = os.path.join(tmp_dir, f'{unique_audio_files[idx]}_stats.wav' )
+    mfcc_src        = os.path.join(input_folder, input_files[idx])
+    mfcc_features   = os.path.join(tmp_dir, f'{input_files[idx]}_features.wav')
+    mfcc_stats      = os.path.join(tmp_dir, f'{input_files[idx]}_stats.wav' )
     # Compute spectral shape descriptors
     subprocess.call([
         'fluid-mfcc', 
@@ -54,12 +60,13 @@ def analyse(idx):
     except:
         print(f'There was no data to process for {mfcc_src}.')
 
-num_jobs = len(unique_audio_files)
+num_jobs = len(input_files)
 
 with mp.Pool() as pool:
     for i, _ in enumerate(
         pool.imap_unordered(analyse, range(num_jobs)), 1):
             sys.stderr.write('\rAnalysis Progress {0:%}'.format(i/num_jobs))
 
-write_json(json_out, dict(mfcc_dict))
-os.rmdir(tmp_dir)
+write_json(output_json, dict(mfcc_dict))
+rmtree(tmp_dir)
+printp('Finished analysis')
